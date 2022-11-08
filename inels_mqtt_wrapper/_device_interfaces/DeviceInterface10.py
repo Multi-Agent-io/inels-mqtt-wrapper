a0da6a79cf7d998151a75d1361480bc3b5a2d076
arseniiarsenii <arseniivelichko2@gmail.com>
2022-11-08 18:25:28 +0300

    feat: Implemented DeviceInterface10

--- inels_mqtt_wrapper/_device_interfaces/DeviceInterface10.py
+++ inels_mqtt_wrapper/_device_interfaces/DeviceInterface10.py
@@ -1,7 +1,6 @@
 from ..interface import AbstractDeviceSupportsStatus, StatusDataType
 
 
-# TODO: Implement device interface
 class DeviceInterface10(AbstractDeviceSupportsStatus):
     """A base class for all the devices implementing the 'device type 10' interface"""
 
@@ -9,4 +8,11 @@ class DeviceInterface10(AbstractDeviceSupportsStatus):
 
     @staticmethod
     def _decode_status(raw_status_data: bytearray) -> StatusDataType:
-        raise NotImplementedError  # TODO: Implement _decode_status() method for class DeviceInterface10
+        data_0, data_2, data_3, data_4, data_5 = raw_status_data
+        temperature_in = int.from_bytes(bytearray((data_2, data_3)), byteorder="little", signed=True) / 100
+        temperature_out = int.from_bytes(bytearray((data_4, data_5)), byteorder="little", signed=True) / 100
+        return {
+            "battery_low": bool(data_0),
+            "temperature_in": temperature_in,
+            "temperature_out": temperature_out,
+        }
