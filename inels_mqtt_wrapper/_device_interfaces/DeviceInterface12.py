3636c24fc31e3d0e03eb21358365f2c4bff6d49a
arseniiarsenii <arseniivelichko2@gmail.com>
2022-11-10 13:04:26 +0300

    fix: Bugfix in byte comparison

--- inels_mqtt_wrapper/_device_interfaces/DeviceInterface12.py
+++ inels_mqtt_wrapper/_device_interfaces/DeviceInterface12.py
@@ -9,8 +9,9 @@ class DeviceInterface12(AbstractDeviceSupportsStatus):
     @staticmethod
     def _decode_status(raw_status_data: bytearray) -> StatusDataType:  # TODO: Testing required
         data_0, data_1, data_2, data_3, data_4 = raw_status_data
+        rftc_status = "RFTC is switched to eLAN mode" if data_2 == int.from_bytes(b"\x80", byteorder="big") else None
         return {
             "battery_low": data_2 % 16 == 1,
-            "rftc_status": "RFTC is switched to eLAN mode" if bytes(data_2) == b"\x80" else None,
+            "rftc_status": rftc_status,
             "temperature": data_0 * 0.5,
         }
