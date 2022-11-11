9fb84c02b757ce9fc35feb75b44e81cade2d618f
arseniiarsenii <arseniivelichko2@gmail.com>
2022-11-11 14:45:45 +0300

    docs: Expanded docstring

--- inels_mqtt_wrapper/utils.py
+++ inels_mqtt_wrapper/utils.py
@@ -2,7 +2,14 @@ from typing import List
 
 
 def extract_bits(byte_data: int, target_len: int = 8) -> List[int]:
-    """Extract bits from the bytes provided"""
+    """
+    Extract bits from the bytes provided.
+
+    :param byte_data: Source bytes as an integer .
+    :param target_len: The desired bitstring length. Defaults to 8.
+    :return: A list of integers of the target length.
+        The members of the list are 0 and 1.
+    """
     bin_num = bin(byte_data).lstrip("0b")
     bits = [int(bit) for bit in bin_num]
     if len(bits) < target_len:
